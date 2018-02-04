b46b4d68037364ce5528059f714504548e5f1fd7
Robin Ronson <robin_ronson@yahoo.com>
2018-02-04 15:12:02 -0600

    fixed errors

---
+++ client/src/containers/LoginPage.jsx
@@ -0,0 +1,73 @@
+import React, { PropTypes } from 'react';
+import LoginForm from '../components/LoginForm.jsx';
+
+
+class LoginPage extends React.Component {
+
+  /**
+   * Class constructor.
+   */
+  constructor(props) {
+    super(props);
+
+    // set the initial component state
+    this.state = {
+      errors: {},
+      user: {
+        email: '',
+        password: ''
+      }
+    };
+
+    this.processForm = this.processForm.bind(this);
+    this.changeUser = this.changeUser.bind(this);
+  }
+
+  /**
+   * Process the form.
+   *
+   * @param {object} event - the JavaScript event object
+   */
+  processForm(event) {
+    // prevent default action. in this case, action is the form submission event
+    event.preventDefault();
+
+    // create a string for an HTTP body message
+    const email = encodeURIComponent(this.state.user.email);
+    const password = encodeURIComponent(this.state.user.password);
+    const formData = `email=${email}&password=${password}`;
+
+  }
+
+  /**
+   * Change the user object.
+   *
+   * @param {object} event - the JavaScript event object
+   */
+  changeUser(event) {
+    const field = event.target.name;
+    const user = this.state.user;
+    user[field] = event.target.value;
+
+    this.setState({
+      user
+    });
+  }
+
+  /**
+   * Render the component.
+   */
+  render() {
+    return (
+      <LoginForm
+        onSubmit={this.processForm}
+        onChange={this.changeUser}
+        errors={this.state.errors}
+        user={this.state.user}
+      />
+    );
+  }
+
+}
+
+export default LoginPage;
--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const app = express();
 // tell the app to look for static files in these directories
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
-// tell the app to parse HTTP body messages
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// routes
-const authRoutes = require('./server/routes/auth');
-app.use('/auth', authRoutes);
 
 // start the server
 app.listen(3000, () => {
