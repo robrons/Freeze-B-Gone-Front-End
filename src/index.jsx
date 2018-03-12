57c5cc3a93cf9e7a78c9996d9a8612a3d153b807
l1mb0 <robin_ronson@yahoo.com>
2018-03-12 10:48:29 -0500

    removed folder

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-var firebase = require('firebase');
-var firebaseui = require('firebaseui')
-
-render(
-  document.getElementById('app'),
-);
