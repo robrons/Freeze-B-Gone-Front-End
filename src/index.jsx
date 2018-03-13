b6891dc2e012029f46c24c82536d97503ae68c8b
l1mb0 <robin_ronson@yahoo.com>
2018-03-13 05:28:45 -0500

    added app bar

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom'
+import SimpleAppBar from 'MenuAppBar.jsx'
 
-render(
-  document.getElementById('app'),
-);
+render(<SimpleAppBar/>, app);
