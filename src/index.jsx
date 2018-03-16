5c038c6162eeda85e6d9dad7bf6bdb8a329afee5
l1mb0 <robin_ronson@yahoo.com>
2018-03-16 18:57:10 -0500

    added states

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom'
 import SignInScreen from 'login.jsx'
+import Home from 'home.jsx'
 import { HashRouter, Route, IndexRoute, hashHistory } from 'react-router-dom'
 
 render(
         <HashRouter history={hashHistory}>
-        <Route path="/" component = {SignInScreen}>
-        </Route>
+        <Route path="/" component={SignInScreen}> </Route>
         </HashRouter>
 , document.getElementById('app'));
--- src/login.jsx
+++ src/login.jsx
@@ -10,28 +10,56 @@ const config = {
   authDomain: 'freeze-b-gone-auth.firebaseapp.com',
   // ...
 };
+
 firebase.initializeApp(config);
 
-// Configure FirebaseUI.
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: 'popup',
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/signedIn',
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ]
-};
-
 export default class SignInScreen extends React.Component {
+
+  constructor() {
+    super()
+    // The component's Local state.
+    this.state = {
+      signedIn: false // Local signed-in state.
+    };
+
+    // Configure FirebaseUI.
+    this.uiConfig = {
+      // Popup signin flow rather than redirect flow.
+      // We will display Google and Facebook as auth providers.
+        signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID
+      ],
+      callbacks: {
+        // Avoid redirects after sign-in.
+        signInSuccess: () => false
+      }
+    };
+  }
+
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(
+      (user) => this.setState({ signedIn: !!user })
+    );
+  }
+
   render() {
+    if (!this.state.signedIn) {
+      return (
+        <div>
+          <ButtonAppBar />
+          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
+        </div>
+      );
+    }
     return (
       <div>
-        <ButtonAppBar/>
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+        <h1>My App</h1>
+        <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+        <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
       </div>
     );
   }
 }
+
