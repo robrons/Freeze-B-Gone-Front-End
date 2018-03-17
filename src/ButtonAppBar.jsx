636e48429e9973b8d020a9d81364ca47810ae45b
l1mb0 <robin_ronson@yahoo.com>
2018-03-16 19:25:10 -0500

    added logout feture

--- src/ButtonAppBar.jsx
+++ src/ButtonAppBar.jsx
@@ -7,6 +7,7 @@ import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
+import firebase from 'firebase';
 
 const styles = {
   root: {
@@ -22,7 +23,7 @@ const styles = {
 };
 
 function handleClose() {
-  console.log('test')
+  firebase.auth().signOut();
 };
 
 function ButtonAppBar(props) {
