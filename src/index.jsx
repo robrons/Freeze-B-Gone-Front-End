49f119a1f64bcedb71eddcfccea5e7233d2ba26a
l1mb0 <robin_ronson@yahoo.com>
2018-03-16 20:05:52 -0500

    fixed login

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,5 @@ import Home from 'home.jsx'
 import { HashRouter, Route, IndexRoute, hashHistory } from 'react-router-dom'
 
 render(
-        <HashRouter history={hashHistory}>
-        <Route path="/" component={SignInScreen}> </Route>
-        </HashRouter>
+      <SignInScreen/>
 , document.getElementById('app'));
