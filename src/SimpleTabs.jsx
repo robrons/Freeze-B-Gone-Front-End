06416a214ce16dadc28258e35b6ddb5b555ae2a0
l1mb0 <robin_ronson@yahoo.com>
2018-03-17 21:23:21 -0500

    added lists

--- src/SimpleTabs.jsx
+++ src/SimpleTabs.jsx
@@ -1,80 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
-import Tabs, { Tab } from 'material-ui/Tabs';
 import GardenIcon from 'material-ui-icons/LocalFlorist';
 import KitchenIcon from 'material-ui-icons/Kitchen';
 import BathRoomIcon from 'material-ui-icons/HotTub';
-import Typography from 'material-ui/Typography';
-import SimpleCard from 'SimpleCard.jsx';
-
-function TabContainer(props) {
-  return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
-      {props.children}
-    </Typography>
-  );
-}
-
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
   },
 });
 
-class ScrollableTabsButtonForce extends React.Component {
-
-  constructor(){
-    super()
-    
-    this.state = {
-      value: 0,
-    };
-  
-    this.handleChange = (event, value) => {
-      this.setState({ value });
-    };
-  }
-  
-
-  render() {
-    const { classes } = this.props;
-    const { value } = this.state;
-
-    return (
-      <div className={classes.root}>
-        <AppBar position="static" color="default">
-          <Tabs
-            value={value}
-            onChange={this.handleChange}
-            scrollable
-            scrollButtons="on"
-            indicatorColor="primary"
-            textColor="primary"
-          >
-            <Tab label="Garden" icon={<GardenIcon />} />
-            <Tab label="Kitchen" icon={<KitchenIcon />} />
-            <Tab label="Bathroom" icon={<BathRoomIcon />} />
-          </Tabs>
-        </AppBar>
-        {value === 0 && <TabContainer><SimpleCard/></TabContainer>}
-        {value === 1 && <TabContainer><SimpleCard/></TabContainer>}
-        {value === 2 && <TabContainer><SimpleCard/></TabContainer>}
-      </div>
-    );
-  }
+function SimpleList(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <List component="nav">
+        <ListItem button>
+          <ListItemIcon>
+            <GardenIcon/> />
+          </ListItemIcon>
+          <ListItemText primary="Garden" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <KitchenIcon />
+          </ListItemIcon>
+          <ListItemText primary="Kitchen" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <BathRoomIcon />
+          </ListItemIcon>
+          <ListItemText primary="BathRoom" />
+        </ListItem>
+      </List>
+    </div>
+  );
 }
 
-ScrollableTabsButtonForce.propTypes = {
+SimpleList.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(ScrollableTabsButtonForce);
+export default withStyles(styles)(SimpleList);
+
--- src/home.jsx
+++ src/home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ButtonAppBar from 'ButtonAppBar.jsx'
-import SimpleTabs from 'SimpleTabs.jsx'
+import SimpleList from 'SimpleTabs.jsx'
 import FloatingActionButton from 'FloatingActionButton.jsx';
 
 function Home() {
 return (<div>
         <ButtonAppBar logout={true}/>
-        <SimpleTabs/>
+        <SimpleList/>
         <FloatingActionButton/>
         </div>
         );
