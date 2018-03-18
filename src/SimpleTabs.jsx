9ad74b0cd3ca0f7225100d1f1c5fa0877a5a60d3
l1mb0 <robin_ronson@yahoo.com>
2018-03-18 16:32:22 -0500

    added expansion panels

--- src/SimpleTabs.jsx
+++ src/SimpleTabs.jsx
@@ -10,75 +10,96 @@ import ExpandMore from 'material-ui-icons/ExpandMore';
 import Collapse from 'material-ui/transitions/Collapse';
 import StarBorder from 'material-ui-icons/StarBorder';
 import ListSubheader from 'material-ui/List/ListSubheader';
+import ExpansionPanel, {
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
+} from 'material-ui/ExpansionPanel';
+import Typography from 'material-ui/Typography';
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
 
 const styles = theme => ({
   root: {
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
+    flexGrow: 1,
   },
-  nested: {
-    paddingLeft: theme.spacing.unit * 4,
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
   },
 });
 
-class NestedList extends React.Component {
+class ControlledExpansionPanels extends React.Component {
 
-  constructor() {
-    super()
-
-    this.state = { open: true };
-
-    this.handleClick = () => {
-      this.setState({ open: !this.state.open });
+  constructor(){
+    super();
+    
+    this.state = {
+      expanded: null,
     };
-    }
+
+    this.handleChange = panel => (event, expanded) => {
+      this.setState({
+        expanded: expanded ? panel : false,
+      });
+    };
+  }
   render() {
     const { classes } = this.props;
+    const { expanded } = this.state;
 
     return (
       <div className={classes.root}>
-        <List
-          component="nav"
-          subheader={<ListSubheader component="div">Devices</ListSubheader>}
-        >
-          <ListItem button>
-            <ListItemIcon>
-             <GardenIcon  /> 
-            </ListItemIcon>
-            <ListItemText inset primary="Garden" secondary="21 °C"/>
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <KitchenIcon />
-            </ListItemIcon>
-            <ListItemText inset primary="Kitchen" secondary="22 °C" />
-          </ListItem>
-          <ListItem button onClick={this.handleClick}>
-            <ListItemIcon>
-            <BathRoomIcon />
-            </ListItemIcon>
-            <ListItemText inset primary="BathRoom" secondary="20 °C" />
-            {this.state.open ? <ExpandLess /> : <ExpandMore />}
-          </ListItem>
-          <Collapse in={this.state.open} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
-              <ListItem button className={classes.nested}>
-                <ListItemIcon>
-                  <StarBorder />
-                </ListItemIcon>
-                <ListItemText inset primary="Starred" />
-              </ListItem>
-            </List>
-          </Collapse>
-
-        </List>
+        <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleChange('panel1')}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>General settings</Typography>
+            <Typography className={classes.secondaryHeading}>I am an expansion panel</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+              maximus est, id dignissim quam.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <ExpansionPanel expanded={expanded === 'panel2'} onChange={this.handleChange('panel2')}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>Users</Typography>
+            <Typography className={classes.secondaryHeading}>
+              You are currently not an owner
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
+              diam eros in elit. Pellentesque convallis laoreet laoreet.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <ExpansionPanel expanded={expanded === 'panel3'} onChange={this.handleChange('panel3')}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>Advanced settings</Typography>
+            <Typography className={classes.secondaryHeading}>
+              Filtering has been entirely disabled for whole web server
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
+              eros, vitae egestas augue. Duis vel est augue.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       </div>
     );
   }
 }
 
-NestedList.propTypes = {
+ControlledExpansionPanels.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(NestedList);
+export default withStyles(styles)(ControlledExpansionPanels);
--- src/home.jsx
+++ src/home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ButtonAppBar from 'ButtonAppBar.jsx'
-import NestedList from 'SimpleTabs.jsx'
+import ControlledExpansionPanels from 'SimpleTabs.jsx'
 import FloatingActionButton from 'FloatingActionButton.jsx';
 
 function Home() {
 return (<div>
         <ButtonAppBar logout={true}/>
-        <NestedList/>
+        <ControlledExpansionPanels/>
         <FloatingActionButton/>
         </div>
         );
