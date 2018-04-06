5e7f99fdb3e4107077172afaabe1d8d4807bf6a9
l1mb0 <robin_ronson@yahoo.com>
2018-04-06 05:43:26 -0500

    added helper folder

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@ import { render } from 'react-dom'
 import SignInScreen from 'login.jsx'
 import Home from 'home.jsx'
 import { HashRouter, Route, IndexRoute, hashHistory } from 'react-router-dom'
+import { Provider } from 'react-redux';
 
 render(
+
       <SignInScreen/>
 , document.getElementById('app'));
